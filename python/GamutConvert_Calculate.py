0b9158c7723285bfb0be6272b0820f99bfc9a22f
Jed Smith <jedypod@gmail.com>
2021-02-05 17:23:06 -0500

    bugfix XYZ chromaticity conversions with non equal energy whitepoint, add presets for XYZ D65 and XYZ D60

--- python/GamutConvert_Calculate.py
+++ python/GamutConvert_Calculate.py
@@ -1,168 +1,93 @@
 import nuke
-float2 = nuke.math.Vector2
 float3 = nuke.math.Vector3
 float3x3 = nuke.math.Matrix3
 
-CONE_RESP_MAT_BRADFORD = float3x3()
-CONE_RESP_MAT_CAT02 = float3x3()
-CONE_RESP_MAT_VONKRIES = float3x3()
-CONE_RESP_MAT_SHARP = float3x3()
-CONE_RESP_MAT_CMCCAT2000 = float3x3()
+def transpose(m):
+    # Transpose (swap rows and columns) of a nuke.math.Matrix3
+    return float3x3(m[0], m[1], m[2], m[3], m[4], m[5], m[6], m[7], m[8])
 
-# From ACESlib.Utilities_Color : 166
-CONE_RESP_MAT_BRADFORD.set(0.89510, -0.75020,  0.03890, 0.26640,  1.71350, -0.06850, -0.16140,  0.03670,  1.02960)
-CONE_RESP_MAT_CAT02.set(0.73280, -0.70360,  0.00300, 0.42960,  1.69750,  0.01360, -0.16240, 0.00610, 0.98340)
+def set_matrix(m):
+    # Populate a nuke.math.Matrix3 with a 3x3 python list (either 3x3 or 1x9)
+    if len(m) is 3 and [isinstance(r, list) for r in m]:
+        m = sum(m, [])
+    o = float3x3(m[0], m[3], m[6], m[1], m[4], m[7], m[2], m[5], m[8])
+    return o
 
-# https://web.stanford.edu/~sujason/ColorBalancing/adaptation.html
-# from S. Bianco. "Two New von Kries Based Chromatic Adapatation Transforms Found by Numerical Optimization."
-CONE_RESP_MAT_VONKRIES.set(0.40024, -0.2263, 0, 0.7076, 1.16532, 0, -0.08081, 0.0457, 0.91822)
-CONE_RESP_MAT_SHARP.set(1.2694, -0.8364, 0.0297, -0.0988, 1.8006, -0.0315, -0.1706, 0.0357, 1.0018)
-CONE_RESP_MAT_CMCCAT2000.set(0.7982, -0.5918, 0.0008, 0.3389, 1.5512, 0.239, -0.1371, 0.0406, 0.9753)
-
-
-def transpose(mtx):
-    # transpose (swap rows to columns) of a 3x3 matrix
-    order = [0, 3, 6, 1, 4, 7, 2, 5, 8]
-    omtx = float3x3()
-    for i in xrange(9):
-        omtx[i] = mtx[order[i]]
-    return omtx
-
-def mult_f33_f33(A, B):
-    # Calculate multiplication of 3x3 matrix A and 3x3 matrix B
-    C = float3x3()
-    C.makeIdentity()
-
-    # Row 1
-    C[0] = (A[0]*B[0] + A[1]*B[3] + A[2]*B[6])
-    C[1] = (A[0]*B[1] + A[1]*B[4] + A[2]*B[7])
-    C[2] = (A[0]*B[2] + A[1]*B[5] + A[2]*B[8])
-    # Row 2
-    C[3] = (A[3]*B[0] + A[4]*B[3] + A[5]*B[6])
-    C[4] = (A[3]*B[1] + A[4]*B[4] + A[5]*B[7])
-    C[5] = (A[3]*B[2] + A[4]*B[5] + A[5]*B[8])
-    # Row 3
-    C[6] = (A[6]*B[0] + A[7]*B[3] + A[8]*B[6])
-    C[7] = (A[6]*B[1] + A[7]*B[4] + A[8]*B[7])
-    C[8] = (A[6]*B[2] + A[7]*B[5] + A[8]*B[8])
-
-    return C;
+def diag(v):
+    # Create a diagonal 3x3 matrix from a 1x3 vector
+    return float3x3(v[0], 0, 0, 0, v[1], 0, 0, 0, v[2])
 
 
-def mult_f3_f33(src, mtx):
-    return float3(mtx[0] * src[0] + mtx[1] * src[1] + 
-    mtx[2] * src[2], mtx[3] * src[0] + mtx[4] * src[1] + 
-    mtx[5] * src[2], mtx[6] * src[0] + mtx[7] * src[1] + 
-    mtx[8] * src[2])
-
-
-def XYZ_2_xyY(XYZ):
-    xyY = float3()
-    divisor = (XYZ[0] + XYZ[1] + XYZ[2])
-    if (divisor == 0.):
-        divisor = 1e-10
-    xyY.set(XYZ[0] / divisor, XYZ[1] / divisor, XYZ[1])
-    return xyY
-
-
-def xyY_2_XYZ(xyY):
-    XYZ = float3()
-    XYZ.set(
-        xyY[0] * xyY[2] / max( xyY[1], 1e-10), \
-        xyY[2], \
-        (1.0 - xyY[0] - xyY[1]) * xyY[2] / max( xyY[1], 1e-10)
-        )
+def xyY_to_XYZ(xyY):
+    # Convert an xyY chromaticity value to XYZ
+    x = xyY[0]
+    y = xyY[1]
+    if len(xyY) is 2:
+        # Assume an xy chromaticity coordinate, use default Y
+        Y = 1.0
+    else:
+        Y = xyY[2]
+    XYZ = float3(x * Y / max(y, 1e-10), Y, (1.0 - x - y) * Y / max(y, 1e-10))
     return XYZ
 
 
-def calculate_cat_matrix(src_xy, des_xy, coneRespMat=CONE_RESP_MAT_BRADFORD):
-    # Calculates and returns a 3x3 Von Kries chromatic adaptation transform 
-    # from src_xy to des_xy using the cone response primaries defined 
-    # by coneRespMat. By default, coneRespMat is set to CONE_RESP_MAT_BRADFORD. 
-    # The default coneRespMat can be overridden at runtime. 
+def calc_cat(src_xy, dst_xy, cat_method='cat02'):
+    # Calculate Von Kries chromatic adaptation transform matrix,
+    # given a source and destination illuminant and CAT method.
+    # Illuminant is given as xy chromaticity coordinates. 
+    # CAT method is the name of one of the below cone response matrices.
 
-    src_xyY = float3(src_xy[0], src_xy[1], 1.0)
-    des_xyY = float3(des_xy[0], des_xy[1], 1.0)
-
-    src_XYZ = xyY_2_XYZ( src_xyY )
-    des_XYZ = xyY_2_XYZ( des_xyY )
-
-    src_coneResp = mult_f3_f33(src_XYZ, coneRespMat)
-    des_coneResp = mult_f3_f33(des_XYZ, coneRespMat)
-
-    vkMat = float3x3()
-    vkMat.set(
-        des_coneResp[0] / src_coneResp[0], 0.0, 0.0,
-        0.0, des_coneResp[1] / src_coneResp[1], 0.0,
-        0.0, 0.0, des_coneResp[2] / src_coneResp[2]
-        )
-
-    cat_matrix = float3x3()
-    coneRespMatInv = coneRespMat.inverse()
-    cat_matrix = coneRespMat * ( vkMat * coneRespMatInv)
+    crmtxs = {
+        "bianco": [[0.8752, 0.2787, -0.1539], [-0.8904, 1.8709, 0.0195], [-0.0061, 0.0162, 0.9899]],
+        "bianco_pc": [[0.6489, 0.3915, -0.0404], [-0.3775, 1.3055, 0.072], [-0.0271, 0.0888, 0.9383]],
+        "bradford": [[0.8951, 0.2664, -0.1614], [-0.7502, 1.7135, 0.0367], [0.0389, -0.0685, 1.0296]],
+        "cat02": [[0.7328, 0.4296, -0.1624], [-0.7036, 1.6975, 0.0061], [0.003, 0.0136, 0.9834]],
+        "cat02_brill_cat": [[0.7328, 0.4296, -0.1624], [-0.7036, 1.6975, 0.0061], [0.0, 0.0, 1.0]],
+        "cmccat2000": [[0.7982, 0.3389, -0.1371], [-0.5918, 1.5512, 0.0406], [0.0008, 0.0239, 0.9753]],
+        "cmccat97": [[0.8951, -0.7502, 0.0389], [0.2664, 1.7135, 0.0685], [-0.1614, 0.0367, 1.0296]],
+        "fairchild": [[0.8562, 0.3372, -0.1934], [-0.836, 1.8327, 0.0033], [0.0357, -0.0469, 1.0112]],
+        "sharp": [[1.2694, -0.0988, -0.1706], [-0.8364, 1.8006, 0.0357], [0.0297, -0.0315, 1.0018]],
+        "von_kries": [[0.40024, 0.7076, -0.08081], [-0.2263, 1.16532, 0.0457], [0.0, 0.0, 0.91822]],
+        "xyz_scaling": [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
+    }
     
-    return cat_matrix
+    cat_method = cat_method.lower().replace(' ', '_')
+    # Return identity matrix if no match
+    if cat_method not in crmtxs.keys():
+        m = float3x3()
+        m.makeIdentity()
+        return m
+
+    crmtx = set_matrix(crmtxs[cat_method])
+
+    # Get XYZ values from xy chromaticity coordinates
+    src_XYZ = xyY_to_XYZ(src_xy)
+    dst_XYZ = xyY_to_XYZ(dst_xy)
+
+    # Calculate source and destination cone response matrices
+    src_crmtx = transpose(crmtx) * src_XYZ
+    dst_crmtx = transpose(crmtx) * dst_XYZ
+
+    von_kries_matrix = diag(dst_crmtx / src_crmtx)
+    cat_mtx = float3x3()
+    cat_mtx = crmtx * (von_kries_matrix * crmtx.inverse())
+    return cat_mtx
 
 
-def RGBtoXYZ(xy, Y=1.0, inverse=False):
-    # given r g b chromaticities and whitepoint, convert RGB colors to XYZ
-    # based on CtlColorSpace.cpp from the CTL source code : 77
-    # param: xy - dict of chromaticity xy coordinates: rxy: float2(x, y) etc
-    # param: Y - luminance of "white" - defaults to 1.0
-    # param: inverse - calculate XYZ to RGB instead
-    r = xy['rxy']
-    g = xy['gxy']
-    b = xy['bxy']
-    w = xy['wxy']
-
-    X = w.x * Y / w.y
-    Z = (1 - w.x - w.y) * Y / w.y
-
-    # Scale factors for matrix rows
-    d = r.x * (b.y - g.y) + b.x * (g.y - r.y) + g.x * (r.y - b.y)
-
-    Sr =    (X * (b.y - g.y) -      \
-            g.x * (Y * (b.y - 1) +  \
-            b.y  * (X + Z)) +       \
-            b.x  * (Y * (g.y - 1) + \
-            g.y * (X + Z))) / d
-    
-    Sg =    (X * (r.y - b.y) +      \
-            r.x * (Y * (b.y - 1) +  \
-            b.y * (X + Z)) -        \
-            b.x * (Y * (r.y - 1) +  \
-            r.y * (X + Z))) / d
-
-    Sb =    (X * (g.y - r.y) -      \
-            r.x * (Y * (g.y - 1) +  \
-            g.y * (X + Z)) +        \
-            g.x * (Y * (r.y - 1) +  \
-            r.y * (X + Z))) / d
-
-    # Assemble the matrix
-    M = float3x3()
-    M.set(  Sr * r.x, Sr * r.y, Sr * (1 - r.x - r.y),
-            Sg * g.x, Sg * g.y, Sg * (1 - g.x - g.y),
-            Sb * b.x, Sb * b.y, Sb * (1 - b.x - b.y))
-    if inverse:
-        M = M.inverse()
-        return M
-    else:
-        return M
-
-
-def get_primaries(node, dst=False):
-    # get colorspace chromaticities xy coordinates from node
-    dst_pri = ['drxy', 'dgxy', 'dbxy', 'dwxy']
-    pri = ['rxy', 'gxy', 'bxy', 'wxy']
-    d = {}
-    for i, p in enumerate(pri):
-        if dst:
-            d[p] = float2(node[dst_pri[i]].getValue()[0], node[dst_pri[i]].getValue()[1])
-        else:
-            d[p] = float2(node[p].getValue()[0], node[p].getValue()[1])
-    return d
-
+def calc_npm(chr):
+    # Calculate a normalized primaries matrix from the specified chromaticity coordinates,
+    # given 2x4 list of xy chromaticity coordinates: red, green, blue, and white
+    def xy_to_xyz(xy):
+        return float3(xy[0], xy[1], 1.0 - xy[0] - xy[1])
+    rxyz, gxyz, bxyz, wxyz = map(xy_to_xyz, chr)
+    wy = chr[3][1]
+    wxyz = float3(wxyz.x / wy, wxyz.y / wy, wxyz.z / wy)
+    np_mtx = float3x3(rxyz[0], gxyz[0], bxyz[0],
+                      rxyz[1], gxyz[1], bxyz[1],
+                      rxyz[2], gxyz[2], bxyz[2])
+    wscale = np_mtx.inverse() * wxyz 
+    np_mtx = transpose(np_mtx * diag(wscale))
+    return np_mtx
 
 
 def start():
@@ -172,71 +97,22 @@ def start():
     identity_mtx.makeIdentity()
 
     invert = node['invert'].getValue()
-    label = node['label']
     cat_method = node['cat_method'].value()
-    src_primaries = get_primaries(node)
-    dst_primaries = get_primaries(node, dst=True)
-    src_colorspace_name = node['src_colorspace_name'].getValue()
-    dst_colorspace_name = node['dst_colorspace_name'].getValue()
-    
-    # Check for XYZ on src / destination
-    enable_src = (src_colorspace_name != 'XYZ')
-    enable_dst = (dst_colorspace_name != 'XYZ')
-    whitepoint_changed = (src_primaries['wxy'][0] != dst_primaries['wxy'][0] or src_primaries['wxy'][1] != dst_primaries['wxy'][1])
 
-    # Get chromatic adaptation method
-    if cat_method == 'Bradford':
-        coneRespMat = CONE_RESP_MAT_BRADFORD
-    elif cat_method == 'cat02':
-        coneRespMat = CONE_RESP_MAT_CAT02
-    elif cat_method == 'vonKries Hunt-Pointer-Estevez D65-Normalized':
-        coneRespMat = CONE_RESP_MAT_VONKRIES
-    elif cat_method == 'cmccat2000':
-        coneRespMat = CONE_RESP_MAT_CMCCAT2000
-    elif cat_method == 'sharp':
-        coneRespMat = CONE_RESP_MAT_SHARP
-    elif cat_method == 'None':
-        coneRespMat = identity_mtx
-        whitepoint_changed = False
+    src_chr = [node[k].getValue() for k in ['rxy', 'gxy', 'bxy', 'wxy']]
+    dst_chr = [node[k].getValue() for k in ['drxy', 'dgxy', 'dbxy', 'dwxy']]
 
-    # Set label
-    if invert:
-        label.setValue('{1} to {0}'.format(node['src_colorspace_name'].getValue(), node['dst_colorspace_name'].getValue()))
-    else:
-        label.setValue('{0} to {1}'.format(node['src_colorspace_name'].getValue(), node['dst_colorspace_name'].getValue()))
+    rgb_to_xyz = calc_npm(src_chr)
+    xyz_to_rgb = calc_npm(dst_chr).inverse()       
 
-    if enable_src:
-        # Calculate Source RGB to XYZ matrix
-        src_rgb_to_xyz_mtx = RGBtoXYZ(src_primaries)
+    cat_mtx = calc_cat(src_chr[3], dst_chr[3], cat_method)
 
-    if enable_dst:
-        # Calculate Destination XYZ to RGB matrix
-        dst_xyz_to_rgb_mtx = RGBtoXYZ(dst_primaries, inverse=True)
-
-    if enable_src and not enable_dst:
-        # Convert from Source RGB to XYZ only
-        mtx = src_rgb_to_xyz_mtx
-
-    if enable_dst and not enable_src:
-        # Convert from XYZ to Destination RGB only
-        mtx = dst_xyz_to_rgb_mtx
-
-    if enable_src and enable_dst:
-        if whitepoint_changed:
-            # Calculate chromatic adaptation matrix
-            cat_mtx = calculate_cat_matrix(src_primaries['wxy'], dst_primaries['wxy'], coneRespMat=coneRespMat)
-            # We will use our own matrix multiplication algorithm since there seems to be precision issues with the nuke.math module :/
-            # mtx = (cat_mtx * src_rgb_to_xyz_mtx) * dst_xyz_to_rgb_mtx
-            mtx = mult_f33_f33(dst_xyz_to_rgb_mtx, mult_f33_f33(cat_mtx, src_rgb_to_xyz_mtx))
-        else:
-            mtx = mult_f33_f33(dst_xyz_to_rgb_mtx, src_rgb_to_xyz_mtx)
+    mtx = (rgb_to_xyz * cat_mtx) * xyz_to_rgb
 
     if invert:
         mtx = mtx.inverse()
 
     node['matrix'].setValue(mtx)
-
-    return True
 
 
 if __name__=='__main__':
